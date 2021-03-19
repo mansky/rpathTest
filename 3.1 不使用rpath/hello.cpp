d12c62efc4b29ecf867af9a5f089df488819ac5f
mantianrong <tianrong-man@li-gong.com>
2021-03-19 16:30:41 +0800

    v0.1

---
+++ 3.1 不使用rpath/hello.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+
+using namespace std;
+int hello::show(){
+    return 1;
+}
+
---
+++ 3.1 不使用rpath/test.cpp
@@ -0,0 +1,17 @@
+
+#include <string>
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+int main(){
+    hello a;
+    cout<<a.show()<<endl;
+    string b;
+    cin >> b;
+    return 1;
+}
+
+
+
+
